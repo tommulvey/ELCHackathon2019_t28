4c6f2b238b5c1e0c5adacaf2c1a024e3dd68fd93
yxue8 <yxue8@pride.hofstra.edu>
2019-10-12 09:03:23 -0400

    endpoint modified

--- python/endpoint/endpoint.py
+++ python/endpoint/endpoint.py
@@ -5,10 +5,11 @@ except ImportError:
 from datetime import datetime
 import time
 from flask import Flask, jsonify
-from flask import request
 import azure.cosmos.documents as documents
 import azure.cosmos.cosmos_client as cosmos_client
 import azure.cosmos.errors as errors
+import collections, functools, operator
+from ast import literal_eval
 
 url = 'https://team28.documents.azure.com/'
 key = 'lVLk1cd6W79qgTbp7DAfs9QoLxFuewOsgVYLrXRHP9QOufaVdbv3IH274fa9gDYKpyoYBg3RlTF3bLjWvTq2TA=='
@@ -19,7 +20,7 @@ container = database.get_container_client('tweets')
 app = Flask(__name__)
 @app.route('/api/cdate', methods=['GET'])
 def get_time():
-    s = '{'
+    s = {}
     for item in container.query_items(
                     query='SELECT tweets.date FROM tweets where tweets.date >= "2019-10-01" and tweets.date <= "2019-11-01"',
                     enable_cross_partition_query=True):
@@ -27,46 +28,12 @@ def get_time():
         head, text, end = jtime.partition('T')
         rtime = datetime.strptime(head, '%Y-%m-%d').timetuple()
         unixt = int(time.mktime(rtime))
-        s = s + str(unixt) + ':' + str(1) + ','
-    s = s[:-1]
-    s = s + '}'
-    res = jsonify(s)
-    res.headers.add('Access-Control-Allow-Origin', '*')
-    return res
-
-@app.route('/api/stats', methods=['GET'])
-def stats():
-    month = str(request.args.get('months', default = "10", type = str))
-    day = str(request.args.get('days', default = "*", type = str))
-    if (day=="*" and month=="") or (month=="*" and day=="*") or (int(month) > 12 or int(month) < 1):
-        return "bad"
-    # scenario 1. all months/days need to be calculated. return
-    if (month!="*" and day=="*"):
-
-        # num posts
-        res = {}
-        for item in container.query_items(
-            query='SELECT VALUE COUNT(1) FROM tweets where tweets.date >= "2019-'+str(month)+'-01" and tweets.date < "2019-'+str(int(month)+1)+'-01"',
-            enable_cross_partition_query=True
-            ):
-            res["posts"]=str(item)
-        #likes
-        for item in container.query_items(
-            query='SELECT VALUE SUM(tweets.likes) FROM tweets where tweets.date >= "2019-'+str(month)+'-01" and tweets.date < "2019-'+str(int(month)+1)+'-01"',
-            enable_cross_partition_query=True
-            ):
-            res["likes"]=str(item)
-        #rts
-        for item in container.query_items(
-            query='SELECT VALUE SUM(tweets.rts) FROM tweets where tweets.date >= "2019-'+str(month)+'-01" and tweets.date < "2019-'+str(int(month)+1)+'-01"',
-            enable_cross_partition_query=True
-            ):
-            res["rts"]=str(item)
-        
-        return res
-
+        strt = str(unixt)
+        if strt in s :
+            s[strt] = s[strt]+1
+        else:
+            s.update({strt:1})
+    return json.dumps(s)
 
 if __name__ == '__main__':
-    app.jinja_env.auto_reload = True
-    app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run(host='0.0.0.0', debug=True, port=80)
+    app.run(debug=True)
